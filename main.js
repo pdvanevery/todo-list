a848f2dc4126790ec68685a794b77f87d45315b6
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2023-02-03 17:58:45 -0800

    completed todo app, organizes uncompleted tasks alphabetically and moves completed tasks to the bottom

--- main.js
+++ main.js
@@ -42,25 +42,25 @@ function refreshList() {
             return -1;
         }
 
-        return a.description < b.description ? -1 : 1
-    })
+        return a.description < b.description ? -1 : 1;
+    });
 
     ITEMS_CONTAINER.innerHTML = "";
 
     for (const item of items) {
         const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
         const descriptionInput = itemElement.querySelector(".item-description");
-        const compeletedInput = itemElement.querySelector(".item-completed"); 
+        const completedInput = itemElement.querySelector(".item-completed"); 
 
         descriptionInput.value = item.description;
-        compeletedInput.checked = item.compeleted;
+        completedInput.checked = item.completed;
         
         descriptionInput.addEventListener("change", () => {
             updateItem(item, "description", descriptionInput.value);
         });
 
-        compeletedInput.addEventListener("change", () => {
-            updateItem(item, "completed", compeletedInput.checked);
+        completedInput.addEventListener("change", () => {
+            updateItem(item, "completed", completedInput.checked);
         });
 
         ITEMS_CONTAINER.append(itemElement);
